811efbbd5b86a8dadac574bdd4d9ac96d736e757
nelsonic <nelson+github@dwyl.com>
2022-11-18 09:08:32 +0000

    update LiveView Config and use https://authdemo.fly.dev for auth #67

--- lib/liveview_chat_web.ex
+++ lib/liveview_chat_web.ex
@@ -37,6 +37,9 @@ defmodule LiveviewChatWeb do
       import Phoenix.Controller,
         only: [get_flash: 1, get_flash: 2, view_module: 1, view_template: 1]
 
+      # https://hexdocs.pm/phoenix_live_view/0.18.1/installation.html
+      import Phoenix.Component
+
       # Include shared imports and aliases for views
       unquote(view_helpers())
     end
@@ -66,6 +69,7 @@ defmodule LiveviewChatWeb do
       import Plug.Conn
       import Phoenix.Controller
       import Phoenix.LiveView.Router
+      import Phoenix.Component
     end
   end
 
