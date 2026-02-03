e6795ab93444f1ec0b7a6e12d8a6965373c578a5
Nelson <194400+nelsonic@users.noreply.github.com>
2026-02-03 16:36:46 +0000

    add lazy_html to deps in mix.exs fixes #280

--- mix.exs
+++ mix.exs
@@ -52,6 +52,7 @@ defmodule LiveviewChat.MixProject do
       {:phoenix_live_view, "~> 1.1.18"},
       {:phoenix_view, "~> 2.0"},
       {:floki, ">= 0.30.0", only: :test},
+      {:lazy_html, ">= 0.1.0", only: :test},
       {:esbuild, "~> 0.2", runtime: Mix.env() == :dev},
       {:telemetry_metrics, "~> 1.0"},
       {:telemetry_poller, "~> 1.0"},
